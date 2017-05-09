a0d5f672c82fa7ec20a744145a98e16781ebc666
Mike Wazovzky <mike.wazovzky@gmail.com>
2017-05-09 12:21:18 +0300

    ES6 testing and coverage

--- log.js
+++ log.js
@@ -1,25 +1,25 @@
 class Log {
     constructor(value = null) {
-        
+        // 
         if(!value) {
             throw new Error('No input provided.');
         }    
-        
+        // 
         if(typeof value !== 'string') {
             throw new Error('String must be provided.');
         }         
         
         this._value = value;
     }
-    
+    // setter
     set value(value) {
         this._value = value;
     }    
-    
+    // getter
     get value() {
         return this._value;
     }
-    
+    // 
     log() {
         console.log(this._value);
         return this._value;
